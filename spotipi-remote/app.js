24ea78ceb03becb5ff0d64996844ab848e8a4b89
tjsumot <tjsumot@spotipi>
2015-01-07 20:36:37 +0100

    Initial app from cg-angular generator.

---
+++ spotipi-remote/app.js
@@ -0,0 +1,23 @@
+angular.module('spotipiRemote', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('spotipiRemote').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('spotipiRemote').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
