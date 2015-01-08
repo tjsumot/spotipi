cd39f2cb7c96b9a4afa7443874ea6dc323da3e08
tjsumot <tjsumot@spotipi>
2015-01-08 13:02:19 +0100

    Formatting.

--- spotipi-remote/app.js
+++ spotipi-remote/app.js
@@ -2,25 +2,25 @@ angular.module('spotipiRemote', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAni
 
 angular.module('spotipiRemote').config(function($stateProvider, $urlRouterProvider) {
 
-    /* Add New States Above */
-    $urlRouterProvider.otherwise('/home');
+  /* Add New States Above */
+  $urlRouterProvider.otherwise('/home');
 
 });
 
-angular.module('spotipiRemote').constant('spServerAddress', 'http://localhost:3000');
+angular.module('spotipiRemote').constant('spServerAddress', 'http://' + window.location.hostname + ':3000');
 
 
 angular.module('spotipiRemote').run(function($rootScope) {
 
-    $rootScope.safeApply = function(fn) {
-        var phase = $rootScope.$$phase;
-        if (phase === '$apply' || phase === '$digest') {
-            if (fn && (typeof(fn) === 'function')) {
-                fn();
-            }
-        } else {
-            this.$apply(fn);
-        }
-    };
+  $rootScope.safeApply = function(fn) {
+    var phase = $rootScope.$$phase;
+    if (phase === '$apply' || phase === '$digest') {
+      if (fn && (typeof(fn) === 'function')) {
+        fn();
+      }
+    } else {
+      this.$apply(fn);
+    }
+  };
 
 });
